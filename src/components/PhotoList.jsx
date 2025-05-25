797f657aa4d9e5bf46eeb26b201e788104bb1d7d
SydNichols <sydneynichols.m@gmail.com>
2025-05-25 14:45:03 -0400

    Updated PhotoList.jsx
    using the HTML photo container, updated the PhotoList container to render Photo components

---
+++ src/components/PhotoList.jsx
@@ -0,0 +1,25 @@
+import Photo from "./Photo";
+
+// HTML photo container as guide - use to render list of Photo components
+
+const PhotoList = ({ photos, title }) => {
+    return (
+        <div className="photo-container">
+            <h2>{title}</h2>
+            <ul>
+                {photos.length > 0 ? (
+                    photos.map(photo => (
+                        <Photo key={photo.id} photo={photo} />
+                    ))
+                ) : (
+                    <li className="not-found">
+                        <h3>No Results Found</h3>
+                        <p>Your search did not return any results. Please try again.</p>
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
+};
+
+export default PhotoList;
